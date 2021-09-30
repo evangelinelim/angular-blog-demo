201ab5026abaab258c36122810dad3e47c98aa7b
evangelinelim <evangeline.lim@allata.com>
2021-09-30 13:45:20 -0600

    added confirm password validator

--- src/app/account/signup/signup.component.ts
+++ src/app/account/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -17,11 +17,22 @@ createSignUpForm() {
     this.signupForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
       password: new FormControl('', Validators.required),
-      confirmPassword: new FormControl('', Validators.required)
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
     })
-  }
+}
+  matchValues(matchTo: string): (AbstractControl) => ValidationErrors | null {
+    return (control: AbstractControl): ValidationErrors | null => {
+      return !!control.parent &&
+        !!control.parent.value &&
+        control.value === control.parent.controls[matchTo].value
+        ? null
+        : { isMatching: false };
+    };
+}
 
   onSubmit() {
     console.log("submitted");
   }
+
+
 }
