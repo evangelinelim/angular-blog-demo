c6dd61ad6362c050b6fb0282e4faa93c2a040c8a
Brandy Guinn <brandy.guinn@allata.com>
2021-10-01 13:44:35 -0500

    adjusted strict syntax for signup

--- src/app/account/signup/signup.component.ts
+++ src/app/account/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -8,6 +8,7 @@ import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators }
 })
 export class SignupComponent implements OnInit {
   signupForm: FormGroup = new FormGroup({});
+  
   constructor() { }
 
   ngOnInit(): void {
@@ -17,18 +18,28 @@ createSignUpForm() {
     this.signupForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]),
       password: new FormControl('', Validators.required),
-      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password', 'confirmPassword')])
     })
 }
-  matchValues(matchTo: string): (AbstractControl : AbstractControl) => ValidationErrors | null {
+  matchValues(key1: string, key2: string): (AbstractControl : AbstractControl) => ValidationErrors | null {  
     return (control: AbstractControl): ValidationErrors | null => {
-      return !!control.parent &&
-        !!control.parent.value &&
-        control.value === control.parent.controls[matchTo].value
-        ? null
-        : { isMatching: false };
+      var controls = control?.parent?.controls as AbstractControl[];
+      if(control?.parent?.value 
+          && controls 
+          && controls[key1 as keyof AbstractControl[]]
+          && controls[key2 as keyof AbstractControl[]]
+        )        
+        {
+          let control1 = controls[key1 as keyof AbstractControl[]] as FormControl;
+          let control2 = controls[key2 as keyof AbstractControl[]] as FormControl;
+          if(control1.value !== control2.value){
+
+            return { isMatching: false };
+        }        
+      } 
+      return null;
     };
-}
+  }
 
   onSubmit() {
     console.log("submitted");
